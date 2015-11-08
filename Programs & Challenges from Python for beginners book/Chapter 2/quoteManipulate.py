2812168c65a79e170d40eb9f7818b07bcc51a799
Ry09 <Ry09@users.noreply.github.com>
2015-11-07 21:54:14 -0500

    Things worked on 07 Nov
    got through ch1 and through about highway through ch 2

---
+++ Programs & Challenges from Python for beginners book/Chapter 2/quoteManipulate.py
@@ -0,0 +1,25 @@
+#Quotation Manipulation
+
+#quote from IBM Chairman, Tomas Watson, in 1943
+
+quote= "I think there is a world market for maybe five computers."
+
+print("Original quote:")
+print(quote)
+
+print("\nIn uppercase:")
+print(quote.upper())
+
+print("\nIn lowercase:")
+print(quote.lower())
+
+print("\nAs a title:")
+print(quote.title())
+
+print("\nWith a minor replacement:")
+print(quote.replace("five", "millions of"))
+
+print("\nOriginal Quote is still:")
+print(quote)
+
+input("\nPress the enter key to exit")
