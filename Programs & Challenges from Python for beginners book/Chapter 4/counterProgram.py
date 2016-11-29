18cea4c74ed0f71e72b146e022fd33be63c61870
Ry09 <xbloodyx9@gmail.com>
2016-11-28 19:07:37 -0500

    Added chapter 4 and created counter program file within

---
+++ Programs & Challenges from Python for beginners book/Chapter 4/counterProgram.py
@@ -0,0 +1,17 @@
+# this is the counter program from CH 4
+# this is used to demonstrate the range function
+
+
+print("Counting:")
+for i in range(10):
+  print(i, end=" ")
+
+print("\n\nCounting by fives:")
+for i in range(0,50,5):
+    print(i, end=" ")
+
+print("\n\nCounting backwards:")
+for i in range(10,0,-1):
+    print(i, end=" ")
+
+input("\n\nPress the enter key to exit.\n")
